0055f3a32c7bd4c5b191879f9020099661d664c3
Alexander Grein <alexander.grein@gmail.com>
2025-01-30 12:28:00 +0100

    Add TYPO3 12 and 13 and bootstrap_package 15

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,8 +13,8 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'templates',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.4.99',
-            'bootstrap_package' => '12.0.0-13.99.99',
+            'typo3' => '11.5.0-13.4.99',
+            'bootstrap_package' => '12.0.0-15.99.99',
         ],
     ],
     'autoload' => [
